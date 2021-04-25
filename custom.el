a5d644a1f61ad09cd9c018d80fe9211ab2c09627
Felipe Gonçalves <felipe@xra.sh>
2021-04-25 01:27:31 -0300

    Add packages

--- custom.el
+++ custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(graphql-mode company web-mode markdown-mode tide go-mode flow-minor-mode rjsx-mode)))
+   '(dockerfile-mode yaml-mode ## color-theme-sanityinc-tomorrow graphql-mode company web-mode markdown-mode tide go-mode flow-minor-mode rjsx-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
