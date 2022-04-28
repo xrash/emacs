75610ce36bd5b9c560d29b3728688aea5a2aff9c
Felipe Gonçalves <felipe@xra.sh>
2022-04-28 15:40:28 -0300

    Add more modes

--- custom.el
+++ custom.el
@@ -4,9 +4,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(solidity-mode flycheck-golangci-lint protobuf-mode graphql clojure-mode cider
-		   (dockerfile-mode yaml-mode ## color-theme-sanityinc-tomorrow graphql-mode company web-mode markdown-mode tide go-mode flow-minor-mode rjsx-mode)
-		   dockerfile-mode yaml-mode ## color-theme-sanityinc-tomorrow graphql-mode company web-mode markdown-mode tide go-mode flow-minor-mode rjsx-mode)))
+   '(lua-mode kotlin-mode racket-mode solidity-mode flycheck-golangci-lint protobuf-mode graphql clojure-mode cider
+	      (dockerfile-mode yaml-mode ## color-theme-sanityinc-tomorrow graphql-mode company web-mode markdown-mode tide go-mode flow-minor-mode rjsx-mode)
+	      dockerfile-mode yaml-mode ## color-theme-sanityinc-tomorrow graphql-mode company web-mode markdown-mode tide go-mode flow-minor-mode rjsx-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
